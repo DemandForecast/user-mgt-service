1f897e79302a578785f6c021552502315cbe9269
PMChamothKaumal <chamoth.20221293@iit.ac.lk>
2025-12-21 14:06:43 +0530

    first commit

---
+++ dbConfig/dbPaths.go
@@ -0,0 +1,13 @@
+package dbConfig
+
+import (
+	mongo "go.mongodb.org/mongo-driver/mongo"
+)
+
+var DATABASE *mongo.Database
+
+
+
+const DATABASE_URL = "mongodb+srv://pmchamoth_db_user:t0rgjOG2h89qexQD@imomgt.y8j6wpi.mongodb.net/?appName=IMOMgt"
+
+const DATABASE_NAME ="Demand-Forecast"
